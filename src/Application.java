eeddc065ea206caecce1b17d649ad9c8c2389a84
Bartek <bartek9393@poczta.onet.pl>
2017-02-08 18:21:55 +0100

    Changed creating frame, added createSingleJButton()

--- src/Application.java
+++ src/Application.java
@@ -1,10 +1,18 @@
 import javax.swing.*;
+import java.awt.*;
 
-public class Application {
+public class Application extends JComponent{
     Frame ramka = new Frame();
-
     void showFrame() {
         JFrame frame = ramka.createFrame("2048");
+        JPanel panel = new JPanel(new GridLayout(4, 4));
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                panel.add(ramka.createSingleJButton(50, 50));
+                System.out.println();
+            }
+        }
+        frame.add(panel);
         frame.setVisible(true);
     }
 
