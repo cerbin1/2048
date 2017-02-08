5a4d614eddcdfdf1c9e214158f2a419c1a258743
Bartek <bartek9393@poczta.onet.pl>
2017-02-08 18:24:14 +0100

    Added JButtons[][]

--- src/Application.java
+++ src/Application.java
@@ -3,12 +3,15 @@ import java.awt.*;
 
 public class Application extends JComponent{
     Frame ramka = new Frame();
+    JButton[][] jButtons;
+
     void showFrame() {
         JFrame frame = ramka.createFrame("2048");
         JPanel panel = new JPanel(new GridLayout(4, 4));
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
-                panel.add(ramka.createSingleJButton(50, 50));
+                jButtons[i][j] = ramka.createSingleJButton(50, 50);
+                panel.add(jButtons[i][j]);
                 System.out.println();
             }
         }
