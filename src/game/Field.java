e700fe9e44071b0e8419ca31fbaefc0c7013d3fc
Bartek <bartek9393@poczta.onet.pl>
2017-02-08 22:20:03 +0100

    Added isEmpty

--- src/game/Field.java
+++ src/game/Field.java
@@ -1,11 +1,12 @@
 package game;
 
 class Field {
-
     private int value;
+    private boolean isEmpty;
 
     Field(int value) {
         this.value = value;
+        isEmpty = false;
     }
 
     public int getValue() {
