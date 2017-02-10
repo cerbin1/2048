3b937c00d259444ed54969d16fc1b16d540eaa36
Bartek <bartek9393@poczta.onet.pl>
2017-02-10 23:41:16 +0100

    Extracted initializeFields()

--- src/game/Fields.java
+++ src/game/Fields.java
@@ -4,6 +4,10 @@ public class Fields {
     private Field[][] fields = new Field[4][4];
 
     Fields() {
+        initializeFields();
+    }
+
+    private void initializeFields() {
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
                 fields[i][j] = new Field(0);
@@ -11,7 +15,7 @@ public class Fields {
         }
     }
 
-    public Field getFields(int x, int y) {
+    public Field getField(int x, int y) {
         return fields[x][y];
     }
 }
