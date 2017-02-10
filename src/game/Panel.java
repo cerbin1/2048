7b3a415d47480abaec9d25d0b35b58939786da8a
Bartek <bartek9393@poczta.onet.pl>
2017-02-10 01:19:19 +0100

    Fixed suming up values

--- src/game/Panel.java
+++ src/game/Panel.java
@@ -19,30 +19,34 @@ class Panel {
             @Override
             public void keyPressed(KeyEvent e) {
                 if (e.getKeyCode() == 40) {
+
                     for (int i = 0; i < 4; i++) {
-                        if (!fields[1][i].isEmpty() && !fields[0][i].isEmpty()) {
-                            if (Integer.parseInt(fields[1][i].getValue()) == Integer.parseInt(fields[0][i].getValue())) {
-                                System.out.println("1 sie zgadza");
-                                fields[1][i].setValue(Integer.toString(Integer.parseInt(fields[1][i].getValue()) + Integer.parseInt(fields[0][i].getValue())));
-                                fields[0][i].setValue("");
-                                fields[0][i].setEmpty(true);
-                            }
-                        } else if (!fields[2][i].isEmpty() && !fields[1][i].isEmpty()) {
-                            if (Integer.parseInt(fields[2][i].getValue()) == Integer.parseInt(fields[1][i].getValue())) {
-                                System.out.println("2 sie zgadza");
-                                fields[2][i].setValue(Integer.toString(Integer.parseInt(fields[2][0].getValue()) + Integer.parseInt(fields[1][i].getValue())));
-                                fields[1][i].setValue(fields[0][i].getValue());
-                                fields[0][i].setValue("");
-                                fields[1][i].setEmpty(true);
-                            }
-                        } else if (!fields[3][i].isEmpty() && !fields[2][i].isEmpty()) {
+                        if (!fields[3][i].isEmpty() && !fields[2][i].isEmpty()) {
                             if (Integer.parseInt(fields[3][i].getValue()) == Integer.parseInt(fields[2][i].getValue())) {
                                 System.out.println("3 sie zgadza");
                                 fields[3][i].setValue(Integer.toString(Integer.parseInt(fields[3][i].getValue()) + Integer.parseInt(fields[2][i].getValue())));
                                 fields[2][i].setValue(fields[1][i].getValue());
                                 fields[1][i].setValue(fields[0][i].getValue());
                                 fields[0][i].setValue("");
-                                fields[2][i].setEmpty(true);
+                                fields[0][i].setEmpty(true);
+
+                                displayValues(fields);
+                            } else if (!fields[3][i].isEmpty() && !fields[2][i].isEmpty()) {
+                                if (Integer.parseInt(fields[2][i].getValue()) == Integer.parseInt(fields[1][i].getValue())) {
+                                    System.out.println("2 sie zgadza");
+                                    fields[2][i].setValue(Integer.toString(Integer.parseInt(fields[2][i].getValue()) + Integer.parseInt(fields[1][i].getValue())));
+                                    fields[1][i].setValue(fields[0][i].getValue());
+                                    fields[0][i].setValue("");
+                                    fields[0][i].setEmpty(true);
+                                    displayValues(fields);
+                                } else if (!fields[1][i].isEmpty() && !fields[0][i].isEmpty()) {
+                                    if (Integer.parseInt(fields[1][i].getValue()) == Integer.parseInt(fields[0][i].getValue())) {
+                                        System.out.println("1 sie zgadza");
+                                        fields[1][i].setValue(Integer.toString(Integer.parseInt(fields[1][i].getValue()) + Integer.parseInt(fields[0][i].getValue())));
+                                        fields[0][i].setValue("");
+                                        fields[1][i].setEmpty(true);
+                                    }
+                                }
                             }
                         }
                         setButtonsValue(jButtons, fields);
@@ -66,6 +70,15 @@ class Panel {
         panel.setFocusable(true);
         panel.requestFocusInWindow();
         return panel;
+    }
+
+    private void displayValues(Field[][] fields) {
+        for (int d = 0; d < 4; d++) {
+            for (int j = 0; j < 4; j++) {
+                System.out.print(fields[d][j].getValue() + ", ");
+            }
+            System.out.println();
+        }
     }
 
     void setButtonsValue(JButton[][] jButtons, Field[][] fields) {
