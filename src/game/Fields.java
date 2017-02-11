7a2ff2b7d7a3afca0fc9732c4721fce5e0ba7fe6
Bartek <bartek9393@poczta.onet.pl>
2017-02-11 13:10:15 +0100

    Added moveDown()

--- src/game/Fields.java
+++ src/game/Fields.java
@@ -1,6 +1,6 @@
 package game;
 
-public class Fields {
+class Fields {
     private Field[][] fields = new Field[4][4];
 
     Fields() {
@@ -15,7 +15,11 @@ public class Fields {
         }
     }
 
-    public Field getField(int x, int y) {
+    Field getField(int x, int y) {
         return fields[x][y];
+    }
+
+    public void moveDown() {
+
     }
 }
