11e0936adabd651d396f7c6fce782bfde5326434
Daniel <wilkowski.kontakt@gmail.com>
2017-02-11 14:17:07 +0100

    Added Direction

---
+++ src/game/Direction.java
@@ -0,0 +1,21 @@
+package game;
+
+public enum Direction {
+    Down(0, -1), Up(0, 1), Left(-1, 0), Right(1, 0);
+
+    private final int x;
+    private final int y;
+
+    Direction(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
